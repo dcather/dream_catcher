ec75145e7a39cbed1fd8c352914c47db88634020
dcather <dcather86@gmail.com>
2012-06-30 20:20:21 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamCatcher::Application.config.secret_token = 'cab11d8fee3cb4aa3c060b4c328bcb3d85d17272b38822802f941cd0533c83999765a54c56c87b007e9537fec965e8a6fcd45cd1b8d47fce77108071b6b9294b'
